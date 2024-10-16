e2a49ba4a44204830e322dee0dee199e62fc067c
psul502 <psully50@gmail.com>
2024-10-16 21:13:17 +0100

    11: added filtering for alphabetical values and reporting of how frequent those values were

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_the_book(book_path)
     wordnum = book_word_num(text)
-    print(wordnum)
     letter_counts = letter_nums(text)
-    print(letter_counts)
+    letter_ls = []
+    # convert dictionary into lists of dictionaries only added dictionaries if the keys are valid alphabetical values
+    for letter in letter_counts: 
+        testy=list(letter) 
+        if testy[0].isalpha() == True: 
+           letter_ls.append({"letter": letter, "num": letter_counts[letter]})
+ 
+    letter_ls.sort(reverse=True, key=sort_ind)
+    print_report(letter_ls,wordnum,book_path)
 
 def get_the_book(path):
     with open(path) as b:
@@ -28,6 +35,18 @@ def letter_nums(text):
                 letter_counts[letter] = 1
     return letter_counts
 
-        
+def sort_ind(dict):
+    return dict["num"]
+
+def print_report(sorted_dict,wordnum,book_path):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{wordnum} words found in the document")
+    print("")
+    for entry in sorted_dict:
+       let = entry["letter"]
+       num = entry["num"]
+       print(f"The {let} character was found {num} time")
+    print("--- End report ---")
+
 
 main()
