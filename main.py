bd00c33167b960fe10fde790bb3d72a1e5a7800b
psul502 <psully50@gmail.com>
2024-10-16 18:32:24 +0100

    10: added count individual character functionality

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ def main():
     text = get_the_book(book_path)
     wordnum = book_word_num(text)
     print(wordnum)
+    letter_counts = letter_nums(text)
+    print(letter_counts)
 
 def get_the_book(path):
     with open(path) as b:
@@ -12,5 +14,20 @@ def book_word_num(text):
     words = text.split()
     wordnum = len(words)
     return wordnum
+
+def letter_nums(text):
+    letter_counts = {}
+    words = text.split()
+    for word in words:
+        word = word.lower()
+        word1 = list(word)
+        for letter in word1:
+            if letter in letter_counts:
+                letter_counts[letter] += 1
+            else:
+                letter_counts[letter] = 1
+    return letter_counts
+
+        
 
 main()
